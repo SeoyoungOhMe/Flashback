76d5348399c868d3f32711fdee09b687f83d3cc7
yuseon-bae <qodbtjs333@gmail.com>
2024-04-09 16:57:04 +0900

    [feat] users 테이블에  db query문 수정

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 졸업프로젝트 그로쓰
 
 ```
-npm install express ejs body-parser pg express-session     
+npm install express ejs body-parser pg express-session bcypt jsonwebtoken
 ```
   
 npm start로 시작!
--- routes/signup/signup.js
+++ routes/signup/signup.js
@@ -28,7 +28,7 @@ router.post('/', async(req, res) => {
 
     try {
         ////nickname, id, passowrd 3개가 다 일치하는 사용자가 db에 이미 존재하는 경우          
-        const result = await db.query('SELECT * FROM member WHERE nickname = $1 AND id = $2 AND password = $3', [nickname, id, password]);
+        const result = await db.query('SELECT * FROM users WHERE nickname = $1 AND id = $2 AND password = $3', [nickname, id, password]);
         if (result.rows.length > 0) {
             return res.status(400).json({ success: false, message: '이미 존재하는 사용자입니다.' });
         }
@@ -37,7 +37,7 @@ router.post('/', async(req, res) => {
         const hashedPassword = await bcrypt.hash(password, saltRounds);
     
         // DB에 회원 정보 저장
-        await db.query('INSERT INTO member (nickname, id, password) VALUES ($1, $2, $3)', [nickname, id, hashedPassword]);
+        await db.query('INSERT INTO users (nickname, id, password) VALUES ($1, $2, $3)', [nickname, id, hashedPassword]);
     
         res.status(201).json({ success: true, message: '회원가입에 성공했습니다.' });
     } catch (error) {
