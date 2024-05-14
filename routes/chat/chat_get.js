36d3e641c12e3a8260dbe9da5bd5ca9268ff77b0
Seoyoung Oh <ohseoyoung5@gmail.com>
2024-05-14 13:52:01 +0900

    [feat] openai api로 질문 10개 받아오는 코드

--- routes/chat/chat_get.js
+++ routes/chat/chat_get.js
@@ -9,9 +9,12 @@ const openai = new OpenAI({
 
 // POST /api/chat/question 경로에 대한 처리
 router.post('/', async (req, res) => {
-    const { title, context, userPrompt } = req.body;
-    console.log(userPrompt)
-    
+    // title : 책 제목
+    // context : 선호 문장
+    // userPrompt : 지피티에게 요청하는 내용
+
+    const {title, context} = req.body;
+
     // 간단히 실패 상황을 시뮬레이션하기 위해 조건을 만들어봅시다.
     if (!title || !context) {
         return res.status(400).json({
@@ -21,12 +24,13 @@ router.post('/', async (req, res) => {
     }
     
     // const generatedQuestion = "이 문장이 당신에게 어떤 의미인가요?";
+    const userPrompt = `${title} 책의 문장 중 하나인 ${context}를 사용자가 좋아하는 이유를 알 수 있는 질문 10개를 생성해줘.`;
 
     try {
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: [{ "role": "user", "content": userPrompt }],
-            max_tokens: 100
+            // max_tokens: 100
         });
         console.log(response.choices[0].message.content);
         res.send(response.choices[0].message.content);
